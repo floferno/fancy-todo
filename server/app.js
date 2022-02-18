c65d0ecd79222d8b5d447e08740c7a325e9f83ee
floferno <olfiarachmi@gmail.com>
2022-02-18 22:17:55 +0800

    Try async await

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-
-const express = require('express')
-const app = express()
-const PORT = 3000
-const router = require('./routes')
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const router = require("./routes");
 // const errorHandler = require('./middlewares/errorHandler')
 // const cors = require('cors')
 
 // app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(router)
-// app.use(errorHandler) 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+// app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`port berjalan dgn kekuatan ${PORT} tenaga kuda`))
+app.listen(PORT, () =>
+  console.log(`port berjalan dgn kekuatan ${PORT} tenaga kuda`)
+);
--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -2,90 +2,194 @@ const { Todo } = require('../models/')
 
 
 class TodoController {
-    static getTodos(req, res) {
-        Todo.findAll()
-        .then(data => {
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
+    // static getTodos(req, res) {
+    //     Todo.findAll()
+    //     .then(data => {
+    //         res.status(200).json(data)
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json(err)
+    //     })
+    // }
+
+    static async getTodos(req, res) {
+        try {
+            const data = await Todo.findAll()
+            if(!data) {
+				throw { msg: "No todo added yet"}
+			} else { 
+				res.status(200).json(data)
+			}
+		} catch(err) {
+            const error = err.msg || 'Internal server error'
+			res.status(500).json({ error })
+		}
     }
 
-    static addTodo(req, res) {
-        const { title, description, status, due_date } = req.body
-        Todo.create({
-            title, description, status, due_date
-        })
-        .then(data => {
-            res.status(201).json(data)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
+    // static addTodo(req, res) {
+    //     const { title, description, status, due_date } = req.body
+    //     Todo.create({
+    //         title, description, status, due_date
+    //     })
+    //     .then(data => {
+    //         res.status(201).json(data)
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json(err)
+    //     })
+    // }
+
+    static async addTodo(req, res) {
+        try {
+            const { title, description, status, due_date } = req.body
+            const data = await Todo.create({ title, description, status, due_date })
+            if(data) res.status(201).json(data)
+		} catch(err) {
+			res.status(500).json(err)
+		}
     }
 
-    static getTodoById(req, res) {
-        Todo.findOne({
+    // static getTodoById(req, res) {
+    //     Todo.findOne({
+    //         where: {
+    //             id: +req.params.id
+    //         }
+    //     })
+    //     .then((data) => {    
+    //         console.log(data)
+    //         res.status(200).json(data)
+    //     })
+    //     .catch((err) => {
+    //         res.status(500).json(err)
+    //     })  
+    // }
+
+
+    static async getTodoById(req, res) {
+		try {
+			const data = await Todo.findOne({
             where: {
                 id: +req.params.id
             }
         })
-        .then((data)=> {    
-            console.log(data)
-            res.status(200).json(data)
-        })
-        .catch((err) => {
-            res.status(500).json(err)
-        })  
+			if(!data) {
+				throw { msg: "Data not found"}
+			} else { 
+				res.status(200).json(data)
+			}
+		} catch(err) {
+            const error = err.msg || 'Internal server error'
+			res.status(500).json({ error })
+		}
     }
 
-    static updateTodo(req, res) {
-        console.log("masuk")
-        const {title, description, status, due_date } = req.body
-        Todo.update({ title, description, status, due_date },{
-            where: {
-                id: +req.params.id
-            },
-            returning: true
-        })
-        .then(data => {
-            res.status(200).json(data[1][0])
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
+	
+    // static updateTodo(req, res) {
+    //     console.log("masuk")
+    //     const { title, description, status, due_date } = req.body
+    //     Todo.update({ title, description, status, due_date },{
+    //         where: {
+    //             id: +req.params.id
+    //         },
+    //         returning: true
+    //     })
+    //     .then(data => {
+    //         res.status(200).json(data[1][0])
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json(err)
+    //     })
+    // }
+
+	static async updateTodo(req, res) {
+		try {
+			const { title, description, status, due_date } = req.body
+			const data = await Todo.update({ title, description, status, due_date }, {
+				where: {
+					id: +req.params.id
+				},
+				returning: true
+			})
+			if(!data) {
+				throw { msg: "Data not found"}
+			} else { 
+				res.status(200).json(data[1][0])
+			}
+		} catch(err) {
+            const error = err.msg || 'Internal server error'
+			res.status(500).json({ error })
+		}
+	}
+
+    // static updateStatus(req, res) {
+    //     const { status } = req.body
+    //     Todo.update({ status }, {
+    //         where: {
+    //             id: +req.params.id
+    //         },
+    //         returning: true
+    //     })
+    //     .then(data => {
+    //         res.status(200).json(data[1][0])
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json(err)
+    //     })
+    // }
+
+	static async updateStatus(req, res) {
+        try {
+			const { status } = req.body
+			const data =  await Todo.update({ status }, {
+				where: {
+					id: +req.params.id
+				},
+				returning: true
+			})
+			if(!data) {
+					throw { msg: "Data not found"}
+				} else { 
+					res.status(200).json(data[1][0])
+				}
+		} catch(err) {
+            const error = err.msg || 'Internal server error'
+			res.status(500).json({ error })
+		}
     }
 
-    static updateStatus(req, res) {
-        const { status } = req.body
-        Todo.update({ status }, {
-            where: {
-                id: +req.params.id
-            },
-            returning: true
-        })
-        .then(data => {
-            res.status(200).json(data[1][0])
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-    }
 
-    static deleteTodo(req, res) {
-        Todo.destroy({
-            where: {
-                id: +req.params.id
-            }
-        })
-        .then(() => {
-            res.status(200).json("Todo successfully deleted")
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-    }
+    // static deleteTodo(req, res) {
+    //     Todo.destroy({
+    //         where: {
+    //             id: +req.params.id
+    //         }
+    //     })
+    //     .then(() => {
+    //         res.status(200).json("Todo successfully deleted")
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json(err)
+    //     })
+    // }
+
+	static async deleteTodo(req, res) {
+		try {
+			const data = await Todo.destroy({
+				where: {
+					id: +req.params.id
+				}
+			})
+			if(!data) {
+				throw { msg: "Data not found"}
+			} else { 
+				res.status(200).json({ msg: "Todo successfully deleted"})
+			}
+		} catch(err) {
+            const error = err.msg || 'Internal server error'
+			res.status(500).json({ error })
+		}
+	}
+
 }
 
 module.exports = TodoController
