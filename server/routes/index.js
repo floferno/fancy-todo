922b35942d959fbd06580011d09d59c6b2e62ebc
floferno <olfiarachmi@gmail.com>
2022-08-20 11:02:32 +0800

    latest updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,9 @@ const TodoRouter = require('./todoRouter')
 const UserRouter = require('./userRouter')
 const APIRouter = require('./APIRouter')
 
+router.get('/', (req, res) => {
+    res.send('Welcome!')
+})
 router.use('/users', UserRouter)
 router.use('/todos', TodoRouter)
 router.use('/api', APIRouter)
