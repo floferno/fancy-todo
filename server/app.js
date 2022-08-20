60f8c192eb9b9472d2347067ac173a4bb6c27b1e
floferno <olfiarachmi@gmail.com>
2022-08-20 15:24:22 +0800

    updates client

--- server/app.js
+++ server/app.js
@@ -8,8 +8,13 @@ const PORT = process.env.PORT || 3000;
 const router = require("./routes");
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
